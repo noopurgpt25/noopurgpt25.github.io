ae2c383a1ac9a680a91a9b7900fd88a71dd916bd
Noopur <noopurgpt25@gmail.com>
2018-02-25 23:51:06 -0600

    P6 computer prototype

--- js/StudyOptimization.js
+++ js/StudyOptimization.js
@@ -10,7 +10,24 @@
       labels: ["HCI-HW", "AI-HW", "AI-READING"],
       datasets: [
       {
-        data: [80, 80, 80],
+        data: [20,20,20],
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
+        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
+      }
+      ]
+    },
+    options: {
+      responsive: true
+    }    
+  });
+   var ctxP2 = document.getElementById("pieChart2").getContext('2d');
+   var myPieChart = new Chart(ctxP2, {
+    type: 'pie',
+    data: {
+      labels: ["HcI-HW-Paper Prototype", "HCI-HW-Reading"],
+      datasets: [
+      {
+        data: [80,60 ],
         backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
         hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
       }
@@ -21,56 +38,6 @@
     }    
   });
 
-
-  var ctxB = document.getElementById("barChart").getContext('2d');
-  var myBarChart = new Chart(ctxB, {
-    type: 'bar',
-    data: {
-      labels: ["HCI-HW-CP1", "AI-Lab3"],
-      datasets: [{
-        label: 'Final Score',
-        data: [80,95,0,100],
-        backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-
-        ],
-        borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-
-        ],
-        borderWidth: 1
-      }]
-    },
-    optionss: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
-      }
-    }
-  });
-
-  var ctxD = document.getElementById("doughnutChart").getContext('2d');
-  var myLineChart = new Chart(ctxD, {
-    type: 'doughnut',
-    data: {
-      labels: ["HcI-HW-Paper Prototype", "HCI-HW-Reading"],
-      datasets: [
-      {
-        data: [20,40],
-        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-        hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-      }
-      ]
-    },
-    options: {
-      responsive: true
-    }    
-  });
 
   $(document).ready(function(){
         var date_input=$('input[name="date"]'); //our date input has the name "date"
